b897a1fc41a8002064fa3bf8ddedccc02556b36b
vipwp@qq.com <vipwp@qq.com>
2014-11-06 01:42:21 +0800

    添加线性表非紧密存储型结构的串

--- struct/String/SqString.py
+++ struct/String/SqString.py
@@ -72,14 +72,82 @@ class SqString(object):
         return result
 
     def sub_str(self, i, j):
-        # 求字串: 返回串s 中从第 i(1<= i <= StrLength(s)) 个字符开始，由连续
+        # 求字串: 返回串s 中从第 i(1<= i <= self.length) 个字符开始，由连续
         # j个字符组成的子串
-        pass
+        # 返回新串
+        if i <= 0 or i > self.length or i + j - 1 > self.length:
+            raise IndexError("Index error!")
+        result = SqString(j)
+        for k, item in enumerate(self.data[i-1:i+j-1]):
+            result.data[k] = item
+            result.length += 1
+        return result
 
     def ins_str(self, i, sqstr):
-        # 将串 sqstr 插入到 该串
+        # 将串 sqstr 插入到 该串的第i 个字符中，即将 sqstr 的第一个字符作为s1的第i个字符，
+        # 并返回返回产生的新串
+        if i <= 0 or i > self.length:
+            raise IndexError("Index is out of range!")
+        result = SqString(self.max_size + sqstr.max_size)
+        #将 self.data[0:i-2] 复制到result中
+        for k, item in enumerate(self.data[0:i-1]):
+            result.data[k] = item
+            result.length += 1
+        #将 sqstr 复制到result中
+        # len2 = result.length
+        for k, item2 in enumerate(sqstr.data):
+            if item2 is not None:
+                result.data[i - 1 + k] = item2
+                result.length += 1
+            else:
+                break
+        #将 self.data[i-1:] 复制到result中
+        # len3 = result.length
+        for k, item3 in enumerate(self.data[i-1:]):
+            if item3 is not None:
+                result.data[i - 1 + sqstr.length + k] = item3
+                result.length += 1
+            else:
+                break
+        return result
 
+    def del_str(self, i, j):
+        # 从串s 中删除第 i (1<= i <= self.length) 个字符开始，长度为j的字串
+        # 并返回产生的新串
+        # 参数不正确时返回一个空串
+        if i <= 0 or i > self.length or i + j - 1 > self.length:
+            raise IndexError("Index is out of range")
+        result = SqString(self.length - j)
+        for k, item in enumerate(self.data[0:i]):
+            result.data[k] = item
+            result.length += 1
+        for k, item in enumerate(self.data[i + j - 1:]):
+            if item is not None:
+                result.data[i - 1 + k] = item
+                result.length += 1
+            else:
+                break
+        return result
 
+    def rep_str(self, i, j, sqstr):
+        # 在串self中，将第i(1<= i <= self.length)个字符开始的j个字符构成的子串用串t替换，
+        # 并返回产生的新串。
+        # 参数不正确是返回一个空串。
+        if i <= 0 or i > self.length or i + j - 1 > self.length:
+            raise IndexError("Index is out of range")
+        result = SqString(self.length - j + sqstr.length)
+        for k, item in enumerate(self.data[0:i]):
+            result.data[k] = item
+            result.length += 1
+        for k, item in enumerate(sqstr.data):
+            if item is not None:
+                result.data[i - 1 + k] = item
+                result.length += 1
+        for k, item in enumerate(self.data[i + j - 1:]):
+            if item is not None:
+                result.data[i - 1 + sqstr.length + k] = item
+                result.length += 1
+        return result
 
 
 if __name__ == '__main__':
@@ -92,3 +160,17 @@ if __name__ == '__main__':
     sqtest.show_str()
     a = sqstr.equal(sqtest)
     print a
+    result = sqstr.sub_str(3, 5)
+    result.show_str()
+
+    sss = "123"
+    test1 = SqString(5)
+    test1.assign(sss)
+    result2 = sqstr.ins_str(3, test1)
+    result2.show_str()
+
+    result3 = result2.del_str(5, 6)
+    result3.show_str()
+
+    result4 = result3.rep_str(5, 4, test1)
+    result4.show_str()
